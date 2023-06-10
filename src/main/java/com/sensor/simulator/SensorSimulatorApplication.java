fc2fd62c0fb214b8e5922f10c559756d6a0572b4
Ehsan Zangeneh <ehsan.zangneh@gmail.com>
2023-06-10 13:45:31 +0200

    kafka congif

--- src/main/java/com/sensor/simulator/SensorSimulatorApplication.java
+++ src/main/java/com/sensor/simulator/SensorSimulatorApplication.java
@@ -1,11 +1,6 @@
 package com.sensor.simulator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SensorSimulatorApplication {
